d3a6a2d18a5c5ccc79961e88ecb7caa1c9a7ea0c
TheVitka <vitka.be@gmail.com>
2023-04-20 00:18:48 +0300

    added author/description/logo

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -4,7 +4,11 @@ module.exports = {
     makers: [
         {
             name: "@electron-forge/maker-squirrel",
-            config: {}
+            config: {
+                author: "Stopter",
+                description: "Electron app for UAVLogViewer with outbound requests and websockets disabled for security.",
+                logo: 'logo.png'
+            }
         },
         {
             name: "@electron-forge/maker-zip",
